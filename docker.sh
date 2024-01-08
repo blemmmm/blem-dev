cd15137a05045883b922a471b9efe30223e79073
blessly.i386g@proton.me <blessly.pera@media-meter.com>
2024-01-08 23:35:23 +0800

    update bash script

--- docker.sh
+++ docker.sh
@@ -2,4 +2,6 @@
 
 if [[ $1 = "hd" ]]; then 
     sudo docker compose up --force-recreate --build hippodirect-typesense hippodirect-server
+else
+    sudo docker compose up --force-recreate --build
 fi
